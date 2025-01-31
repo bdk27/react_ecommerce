99cf1555a960d6be1678a081a24b659818760845
YCheng <briansam195@gmail.com>
2025-01-31 18:07:46 +0800

    引入google-fonts字體

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grey: {
+          light: "#f7f7f7",
+          dark: "#939292",
+        },
+        black: {
+          default: "#2D2D2D",
+        },
+      },
+      fontFamily: {
+        noto: ["Noto Sans TC", "sans-serif"],
+        playfair: ["Playfair Display", "serif"],
+      },
+    },
   },
   plugins: [],
 };
