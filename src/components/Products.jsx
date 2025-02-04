8da6833ab53adbe79809bed14557b3dd961b8534
YCheng <briansam195@gmail.com>
2025-02-04 22:57:30 +0800

    新增Product與Category.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,11 +3,19 @@ import { useSelector } from "react-redux";
 function Products() {
   const categories = useSelector((state) => state.category.categories);
   return (
-    <ul>
-      {categories.map((item) => (
-        <li key={item.id}>{item.name}</li>
-      ))}
-    </ul>
+    <>
+      <div className="p-3 text-white bg-black rounded-t">所有產品</div>
+      <ul>
+        {categories.map((item) => (
+          <li
+            key={item.id}
+            className="p-2 border-b cursor-pointer hover:bg-grey-light"
+          >
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,8 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 function Category() {
+  const routerLinks = [
+    { name: "Home", link: "/" },
+    { name: "TShirts", link: "/category/t-shirts" },
+    { name: "Polos", link: "/category/polos" },
+    { name: "Jackets", link: "/category/jackets" },
+    { name: "Knitwear", link: "/category/knitwear" },
+    { name: "Shirts", link: "/category/shirts" },
+    { name: "Shorts", link: "/category/shorts" },
+    { name: "Jeans", link: "/category/jeans" },
+    { name: "Suits", link: "/category/suits" },
+    { name: "Casual", link: "/category/casual" },
+  ];
+
   return (
     <>
+      {/* <nav>
+        {routerLinks.map((item) => (
+          <Link to={item.link} key={item.name}>
+            {item.name}
+          </Link>
+        ))}
+      </nav> */}
+
       <Outlet />
     </>
   );
