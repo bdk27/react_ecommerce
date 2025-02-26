22eb2fc8136d33271c2e2362855cb6342f2d453c
YCheng <briansam195@gmail.com>
2025-02-26 09:47:55 +0800

    新增產品頁面圖片顯示，並更新相關佈局

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
+import products_image from "@/assets/images/products_image.jpg";
 
 function Products() {
   const categories = useSelector((state) => state.category.categories);
@@ -17,25 +18,34 @@ function Products() {
   }, [location.pathname, categories]);
 
   return (
-    <div className="hidden bg-white md:block">
-      <div className="p-3 text-white bg-black rounded-t">所有產品</div>
-      <ul>
-        {categories.map((item) => (
-          <li
-            key={item.id}
-            className={`p-2 border-b cursor-pointer ${
-              selectedCategory === item.id
-                ? "bg-grey-light dark:bg-dark-mode-grey-200"
-                : "hover:bg-grey-light dark:hover:bg-dark-mode-grey-200"
-            }`}
-          >
-            <Link to={item.link} className="block w-full h-full">
-              {item.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="hidden bg-white md:block">
+        <div className="p-3 text-white bg-black rounded-t">所有產品</div>
+        <ul>
+          {categories.map((item) => (
+            <li
+              key={item.id}
+              className={`p-2 border-b cursor-pointer ${
+                selectedCategory === item.id
+                  ? "bg-grey-light dark:bg-dark-mode-grey-200"
+                  : "hover:bg-grey-light dark:hover:bg-dark-mode-grey-200"
+              }`}
+            >
+              <Link to={item.link} className="block w-full h-full">
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="mt-3">
+        <img src={products_image} alt="products_image" className="rounded-lg" />
+        <p className="text-sm text-grey-dark font-playfair">
+          Origins SPRING SUMMER 2025
+        </p>
+      </div>
+    </>
   );
 }
 
